843eb0bae3f8d897a36c8bed6b366f2547481727
cotrader <33347299+cotrader@users.noreply.github.com>
2025-09-18 20:41:50 +0200

    initial commit

---
+++ AI_Versuch.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+
+
+# Set up Streamlit app
+st.set_page_config(layout="wide")
+st.title("AI-Powered Technical Stock Analysis Dashboard")
+st.sidebar.header("Configuration")
+
+
+
+            
